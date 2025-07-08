ff4501fc5a12ff4c2e0ddb69df397de2eab0b461
yoownny <kelosy00@naver.com>
2025-07-08 20:44:38 +0900

    feat: ChatMessageDto 생성

--- src/main/java/com/ssafy/websocket/config/WebSocketConfig.java
+++ src/main/java/com/ssafy/websocket/config/WebSocketConfig.java
@@ -11,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket // WebSocket 기능 활성화
 @RequiredArgsConstructor // final 필드 생성자 자동 생성
 public class WebSocketConfig implements WebSocketConfigurer {
+    // 실제 웹소켓 요청을 처리할 핸들러 객체 -> WebSocketChatHandler
     private final WebSocketHandler webSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 어떤 경로에 어떤 핸들러를 연결할지 설정 정보를 저장
+
         registry.addHandler(webSocketHandler, "/ws/conn")
                 .setAllowedOrigins("*");
     }
---
+++ src/main/java/com/ssafy/websocket/dto/ChatMessageDto.java
@@ -0,0 +1,23 @@
+package com.ssafy.websocket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessageDto {
+    private MeetingType meetingType; // 메시지 타입
+    private Long chatRoomId; // 방 번호
+    private String message; // 메시지
+
+    // 메시지 타입 : 입장, 채팅, 퇴장
+    public enum MeetingType {
+        JOIN, TALK, LEAVE
+    }
+}
